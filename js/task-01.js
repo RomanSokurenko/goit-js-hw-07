05b03dad678b6c0130a923b464e509eddab4a2d1
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-04-21 08:41:09 +0300

    save21.04

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-const navEl = document.querySelector('li');
-console.log(navEl);
+// const listCategoriesRef = document.querySelector('ul');
+// console.log(listCategoriesRef);
+
+const listRef = document.querySelectorAll('.item');
+console.log(`В списке ${listRef.length} категории.`);
+
+const categoriesRef = document.querySelectorAll('.item');
+console.log(categoriesRef);
+console.log(categoriesRef[0].dataset.categoriesRef);
