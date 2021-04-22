6c858ce1cfadd9b61c2ee7b1f146b390d0e52324
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-04-22 20:46:02 +0300

    save

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,36 @@ console.log(`В списке ${listRef.length} категории.`);
 
 const elements = document.querySelectorAll('h2');
 elements.forEach(el => console.log(`Категория: ${el.innerText}`)
-
 );
+const total = document.querySelectorAll('ul');
+total.forEach(el => console.log(`Количество элементов:${total.length}`));
+
+// const total = document.querySelectorAll('li');
+// const totalCat = total[0].querySelectorAll('li')
+// console.log(`Количество элементов:${totalCat.length}`);
+ 
+
+
+
+//const element = document.querySelector('.item');
+//const qwe = (element.lastElementChild.);
+//console.log(elements.length);
+
+
+
+//console.log(total[1]);
+
+//total.forEach(total => { console.log(`Количество элементов: ${total}`); });
+
+// const total = document.querySelectorAll('.item');
+// console.log(total);
+
+// const total1 = document.querySelectorAll('ul');
+//  console.log(total1);
+
+// const firstEl = total[0].querySelectorAll('li');
+// console.log(`Количество элементов:${firstEl.length}`);
+  
 
 
 
---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+// почти рабочий вариант(((
+// const titleRef = document.createElement('ul');
+// const navTitle = document.createElement('li');
+// navTitle.textContent =ingredients ;
+
+// titleRef.appendChild(navTitle);
+// console.log(titleRef);
+
+
