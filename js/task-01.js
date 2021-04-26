320fed32ac110fde12fa211f56f4e7c0553cea12
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-04-26 08:45:45 +0300

    save

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,25 @@
 const listRef = document.querySelectorAll('.item');
 console.log(`В списке ${listRef.length} категории.`);
 
+
+
 listRef.forEach(el => {
-   const elements = document.querySelector('h2');
-elements.forEach(el => console.log(`Категория: ${el.innerText}`)
-    );
- const elem = document.querySelector('li');
- elem.forEach(el => console.log(`Категория: ${el.length}`)
- );   
-})
+   const element = el.querySelector('ul');
+   const catRel = element;
+   console.log(`Категория: ${catRel}`);
 
+});
 
+// const element1 = document.querySelector('li');
+// console.log(element1.innerText);
+// listRef.forEach(el => {
+//    const elements = document.querySelector('h2');
+// elements.forEach(el => console.log(`Категория: ${el.innerText}`)
+//     );
+//  const elem = document.querySelector('li');
+//  elem.forEach(el => console.log(`Категория: ${el.length}`)
+//  );   
+// })
 
 // const elements = document.querySelectorAll('h2');
 // elements.forEach(el => console.log(`Категория: ${el.innerText}`)
@@ -21,14 +30,9 @@ elements.forEach(el => console.log(`Категория: ${el.innerText}`)
 // const totalCat = total[0].querySelectorAll('li')
 // console.log(`Количество элементов:${totalCat.length}`);
  
-
-
-
 //const element = document.querySelector('.item');
 //const qwe = (element.lastElementChild.);
 //console.log(elements.length);
-
-
 
 //console.log(total[1]);
 
