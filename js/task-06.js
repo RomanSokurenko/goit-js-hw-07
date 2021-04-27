44b44d47694cb72d03ee3e661e63569603f9304e
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-04-27 11:31:10 +0300

    save

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,12 @@
-let inputVal = document.getElementById("validation-input");
+const inputEl = document.querySelector('#validation-input');
 
-let totalLenght = inputVal.getAttribute("data-length");
-let intTotallenght = parseInt(totalLenght, 10);
-
-inputVal.oninput = function() {
-  if (inputVal.value.length === intTotallenght) {
-    inputVal.classList.remove("invalid");
-    inputVal.classList.add("valid");
-  }
-  if (inputVal.value.length === 0) {
-    inputVal.classList.remove("valid");
-    inputVal.classList.remove("invalid");
-  }
-  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-    inputVal.classList.add("invalid");
+const validationInput = function () {
+    console.log(this.value.length);
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList = 'valid';
+  } else {
+    inputEl.classList = 'invalid';
   }
 };
+
+inputEl.addEventListener('blur', validationInput);
